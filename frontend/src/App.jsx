09889f76715f0e317fcdaf4b1945a2569d77e84e
BriArl <brianna.youte@gmail.com>
2025-09-18 15:32:40 -0400

    set up App,jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import GarmentDashboard from "./components/GarmentDashboard";
+
+export default function App() {
+    return (
+        <div>
+            <h1>Fabric Efficiency Dashboard</h1>
+            <GarmentDashboard />
+        </div>
+    );
+}
